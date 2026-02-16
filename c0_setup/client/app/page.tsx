11738a5b336f2bdb55a6e929ba88d30f71fcdd93
JunhaDex <dk.junha.dev@gmail.com>
2026-02-17 00:41:31 +0900

    add c0 setup

---
+++ c0_setup/client/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+      <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+        <h1>FE Client (Next.js)</h1>
+        <p>Running on Kubernetes Cluster 🚀</p>
+        <p>Build with Docker & Minikube</p>
+      </div>
+  );
+}
